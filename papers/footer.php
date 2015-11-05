975a1626b8895d98364d5d29b60e6b0eee654cfe
Keira Owens <u1052145@umail.usq.edu.au>
2015-11-05 11:58:52 +1000

    Create footer.php

---
+++ papers/footer.php
@@ -0,0 +1,11 @@
+!-- Secondary footer navigation -->
+    <div id="navfoot">
+      <a href="../index.html" title="Back to the Homepage"> Home </a>
+      <a href="../program/program.html" title="List of Program">Workshop Program</a>
+      <a href="../registration/registration.html" title="Current Page (registration)">Registration</a>
+      <a href="../program/accommodation.html" title="Hotels in Rio de Janeiro">Accommodation</a>
+      <a href="../tourism/tourist.html" title="Things to see and do in Rio de Janeiro">Tourism</a>
+      <a href="../sitemap.html" title="Site map of all pages (documentation for CSC2406 requirements)"> Site Map</a>
+    </div>
+  </body>
+</html>
